3346e16f1a5fa0267138eefa889021a45c3f0a38
Virginia Leticia <vlafonso@hotmail.com>
2021-09-08 09:41:47 -0300

    Add classe ContaCorrente

---
+++ myBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaVirginia = new ContaCorrente();
+
+            contaDaVirginia.titular = "Barbara";
+            contaDaVirginia.agencia = 1234;
+            contaDaVirginia.conta = 789;
+            contaDaVirginia.saldo = 100.50;
+            
+            ContaCorrente contaDoMateus = new ContaCorrente();
+
+            contaDoMateus.titular = "Mateus";
+            contaDoMateus.agencia = 1234;
+            contaDoMateus.conta = 790;
+            contaDoMateus.saldo = 120;
+
+            Console.WriteLine("O titular da conta é: "+ contaDaVirginia.titular);
+            Console.WriteLine("O titular da conta é: "+ contaDoMateus.titular);
+        }
+    }
+}
